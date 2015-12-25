a265fc161f6942fd35e645dbd8286b6af9c12002
AlicT <alic.trossman@gmail.com>
2015-12-25 01:55:22 -0500

    two errors were fixed

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -1,9 +1,9 @@
 describe 'A broken program' do
 
-  it 'raises an error when loaded' do
+  it 'not to raise an error when loaded' do
     expect{
       load 'lib/a_broken_program.rb'
-    }.to raise_error
+    }.not_to raise_error
   end
 
 end
