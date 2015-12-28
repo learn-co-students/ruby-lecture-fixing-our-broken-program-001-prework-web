e973efc79b9c0da722e012beb3a30b982f14bb04
saraahmad1114 <saraahmad1114@yahoo.com>
2015-12-28 21:49:15 +0000

    Done.

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,4 +1,6 @@
-puts Hello World! # Correct this to "Hello World!"
-
+puts "Hello World!"
+# Correct this to "Hello World!"
 # Your entire program should read:
 # puts "Hello World!"
+#it was only missing the quotations because hello world is a string and must be enclosed in quotations to show such
+#only numerical values aren't enclosed in quotations unless you want to make them into strings
