7d4a82dc257706fa63852e0abd5a5a1c355f501e
anascoding <coding.anas@gmail.com>
2016-03-14 23:44:59 +0200

    Done.

--- spec/a_working_program_spec.rb
+++ spec/a_working_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A working program' do
   it 'not to raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
-    }.to_not raise_error
+    } .to_not raise_error
   end
 
 end
