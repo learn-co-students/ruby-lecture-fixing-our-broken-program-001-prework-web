34b52521a2b86a892a790a12b6b49fafc688abbe
Stefanie Charles <apifanny@aol.com>
2016-05-26 10:13:41 -0400

    Done.

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -2,7 +2,7 @@ describe 'A broken program' do
 
   it 'raises an error when loaded' do
     expect{
-      load 'lib/a_broken_program.rb'
+      load 'lib/a_working_program.rb'
     }.to raise_error
   end
 
